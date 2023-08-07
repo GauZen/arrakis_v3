73f5b49afabd178111f1cd8df2cd289105a8ca80
cen <cen.gao@outlook.com>
2023-08-07 13:38:43 +0100

    junit

--- java-api/src/test/java/com/db/grad/javaapi/model/BondTest.java
+++ java-api/src/test/java/com/db/grad/javaapi/model/BondTest.java
@@ -1,5 +1,16 @@
 package com.db.grad.javaapi.model;
 
+import org.junit.Before;
+
 class BondTest {
+    private Bond bond;
+
+    @Before
+    void setUp(){
+        // create
+        bond = new Bond();
+        bond.setTRADE_CURRENCY("USD");
+    }
+
 
 }
