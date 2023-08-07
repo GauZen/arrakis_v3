15878e70ca99cfcd1d9bb90a99cb05d264c15883
cen <cen.gao@outlook.com>
2023-08-07 12:57:10 +0100

    added login page

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,24 +1,33 @@
+import "bootstrap/dist/css/bootstrap.min.css"
 import React, { useState } from "react";
-import AllBonds from "./components/bonds/AllBonds";
+import "./App.css"
+/* import AllBonds from "./components/bonds/AllBonds"; */
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import Auth from "./components/Auth"
 
 
 function App() {
   const [checked, setChecked] = useState(false);
 
-  const handleChange =() => {
+  const handleChange = () => {
     setChecked(!checked);
   };
 
   return (
     <div className="container" key="app">
-      <label htmlFor="checkout">
-        <input type="checkbox" name="checkbox" id="checkbox" 
-        checked= {checked}
-        onChange={handleChange}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+        </Routes>
+      </BrowserRouter>
+{/*       <label htmlFor="checkout">
+        <input type="checkbox" name="checkbox" id="checkbox"
+          checked={checked}
+          onChange={handleChange}
         />
         Show Bonds by Maturity within the last and next 5 days
       </label>
-      <AllBonds status={checked} key="app-allbonds" />
+      <AllBonds status={checked} key="app-allbonds" /> */}
     </div>
   );
 }
